1a0f04949e4b2701075ba5c3a75e75b699107407
eyesee1 <eyesee1@me.com>
2017-06-05 16:57:01 -0500

    auth token support

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        #'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',

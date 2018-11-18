b4836cf202dc24713ad98148268a724cfef303c5
ic <eyesee1@me.com>
2018-11-18 16:31:20 -0600

    Bump version: 0.2.2 → 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ author = "Isaac Csandl"
 # built documents.
 #
 # The short X.Y version.
-version = "0.2.2"
+version = "version = '0.3.0'"
 # The full version, including alpha/beta/rc tags.
 release = version
 

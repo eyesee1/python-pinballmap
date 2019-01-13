9f69636e1a5b6f40973ac112e403892b214d307a
ic <eyesee1@me.com>
2019-01-13 15:14:47 -0600

    hopefully fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ source_suffix = ".rst"
 master_doc = "index"
 
 # General information about the project.
-project = "python-pinballmap"
+project = "pinballmap"
 copyright = "2017, Duna Csandl"
 author = "Duna Csandl"
 

98bf3549b7943254cb2d1cea79594e733dc5ddd6
Duna C. <marinas.bobble-05@icloud.com>
2022-01-22 19:20:18 -0600

    re-organize code & fix response code error
    - organize code a bit
    - change from requirements.txt to Poetry
    - fix error where client had wrong expectation of response codes from pinball map API

---
+++ pinballmap/auth.py
@@ -0,0 +1,24 @@
+from functools import wraps
+
+from pinballmap.exceptions import PinballMapAuthenticationFailure, TokenRequiredException
+
+
+def requires_authorization(f):
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        if not self.authentication_token and self.user_email:
+            if self.user_email and self.user_password:
+                # we have credentials, why not try them?
+                try:
+                    self.auth_details(
+                        self.user_email, self.user_password, update_self=True
+                    )
+                    return f(self, *args, **kwargs)
+                except PinballMapAuthenticationFailure:
+                    pass
+            raise TokenRequiredException(
+                "Pinball Map authentication_token and user_email required for this operation."
+            )
+        return f(self, *args, **kwargs)
+
+    return wrapper
---
+++ pinballmap/constants.py
@@ -0,0 +1,3 @@
+VERSION = "0.3.4"
+STRIP_WORDS = ("the", "and", "for", "with", "a", "of")
+MODEL_ENDINGS = ("le", "pro", "premium", "edition" "standard")

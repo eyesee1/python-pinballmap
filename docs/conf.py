154990757ba5b32ea9f7f0eedf18e0c96654a9cb
Duna C <marinas.bobble-05@icloud.com>
2024-07-30 17:04:16 -0500

    last try to get rtd theme on rtd lol

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,12 @@ sys.path.insert(0, os.path.join(os.path.abspath("."), "../pinballmap/"))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.githubpages"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.githubpages",
+    "sphinx_rtd_theme",
+    "sphinxcontrib.jquery",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -64,7 +69,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -128,7 +133,7 @@ latex_documents = [
         master_doc,
         "python-pinballmap.tex",
         "python-pinballmap Documentation",
-        "Isaac Csandl",
+        "Duna Csandl",
         "manual",
     )
 ]

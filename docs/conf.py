92bc91c013b9d7142527fe01772cfb2f589d64a2
Duna C. <marinas.bobble-05@icloud.com>
2024-07-30 13:47:15 -0500

    Bump version: 0.4.2 → 0.4.3

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ author = "Isaac Csandl"
 # built documents.
 #
 # The short X.Y version.
-version = "0.4.2"
+version = "0.4.3"
 # The full version, including alpha/beta/rc tags.
 release = version
 
